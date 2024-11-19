49cec496beffdfb0711f6a35debdf740f1da80b1
Teriyaki3712 <dteruya8787@stu.neisd.net>
2024-11-19 17:51:04 -0600

    Dashboard thing

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.Intake;
 
 import org.littletonrobotics.junction.Logger;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -8,6 +9,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Intake extends SubsystemBase {
     private IntakeIO io;
     private IntakeInputsAutoLogged inputs = new IntakeInputsAutoLogged();
+    private boolean Intake1;
 
     public Intake(IntakeIO io) {
         this.io = io;
@@ -18,6 +20,8 @@ public class Intake extends SubsystemBase {
     public void periodic() {
         Logger.processInputs("intake", inputs);
         io.updateInputs(inputs);
+        SmartDashboard.putBoolean("Intakemotor", true);
+        Intake1 = SmartDashboard.getBoolean("Intakemotor", true);
     }
 
     public void SetMoterPower(double power) {
--- src/main/java/frc/robot/subsystems/Securemotors/SecureMotors.java
+++ src/main/java/frc/robot/subsystems/Securemotors/SecureMotors.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.Securemotors;
 
 import org.littletonrobotics.junction.Logger;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -8,6 +9,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class SecureMotors extends SubsystemBase {
     private SecureMotorsIO io;
     private SecureMotorsInputsAutoLogged inputs = new SecureMotorsInputsAutoLogged();
+    private double RMP;
 
     public SecureMotors(SecureMotorsIO io) {
         this.io = io;
@@ -17,7 +19,10 @@ public class SecureMotors extends SubsystemBase {
     public void periodic() {
         Logger.processInputs("Motors", inputs);
         io.updateInputs(inputs);
+        SmartDashboard.putNumber("RPMofmotor", 1);
+        RMP = SmartDashboard.getNumber("RPMofmotar", 1);
     }
+    
 
     public void SetMotorPower(double power) {
         Logger.recordOutput("motor1", power);
