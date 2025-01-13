1b2db91e0d6a5f5d97dc8be645a9eeeb94002568
Teriyaki3712 <dteruya8787@stu.neisd.net>
2025-01-13 17:36:39 -0600

    build constants fixed

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Blinky-Real-Thing";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "a96c0596a34b7b790f0394c243d84fff7e46ae82";
-  public static final String GIT_DATE = "2024-10-20 22:21:14 EDT";
+  public static final int GIT_REVISION = 17;
+  public static final String GIT_SHA = "f6505b7c2acef4a27dc0985306f4e2eeb3a731ad";
+  public static final String GIT_DATE = "2025-01-13 18:35:05 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-10-21 19:09:02 EDT";
-  public static final long BUILD_UNIX_TIME = 1729552142420L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-01-13 18:35:38 EST";
+  public static final long BUILD_UNIX_TIME = 1736811338435L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
